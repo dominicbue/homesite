7b97ffc52868cfe0053f3ef109be3e6119b18f65
DominicBürgin <dominic_bue@gmx.ch>
2023-07-10 11:52:58 +0200

    Injection der Entity ausgebaut

--- cookbook-adapter-persistence/src/main/java/domin/homesite/cookbook/adapterpersistence/domain/CategoryMapper.java
+++ cookbook-adapter-persistence/src/main/java/domin/homesite/cookbook/adapterpersistence/domain/CategoryMapper.java
@@ -5,9 +5,8 @@ import domin.homesite.cookbook.recipemanagement.domain.Category;
 import javax.inject.Inject;
 
 public class CategoryMapper {
-    @Inject
-    private CategoryEntity entity;
-    public CategoryEntity mapDomainToEntity(Category category) {
+
+    public CategoryEntity mapDomainToEntity(Category category, CategoryEntity entity) {
         entity.setObject_Id(category.getCategoryId());
         entity.setName(category.getCategoryName());
         return entity;
