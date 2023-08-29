5ff0c77f6f04dfea6a5985f49c09db764b6f0ed5
DominicBürgin <dominic_bue@gmx.ch>
2023-08-29 14:59:37 +0200

    Cookboook: Maven-Surefire-Plugin integriert

---
+++ cookbook-recipe-management/src/test/java/domin/homesite/cookbook/recipemanagement/domain/DummyTest.java
@@ -0,0 +1,13 @@
+package domin.homesite.cookbook.recipemanagement.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    void dummyForIntegration () {
+        assertTrue(true);
+    }
+}
