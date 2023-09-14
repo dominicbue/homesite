ef69261b1e158d3fd478cf697afb1723b98f5d8e
DominicBürgin <dominic_bue@gmx.ch>
2023-09-14 17:20:08 +0200

    Cookboook: recipe-management changed to CDI

--- cookbook-recipe-management/src/main/java/domin/homesite/cookbook/recipemanagement/application/RecipeManagerFacade.java
+++ cookbook-recipe-management/src/main/java/domin/homesite/cookbook/recipemanagement/application/RecipeManagerFacade.java
@@ -2,25 +2,22 @@ package domin.homesite.cookbook.recipemanagement.application;
 
 import domin.homesite.cookbook.recipemanagement.application.recipe.RecipeLogic;
 import domin.homesite.cookbook.recipemanagement.domain.Recipe;
-import lombok.extern.log4j.Log4j2;
+import lombok.AllArgsConstructor;
 
 import javax.inject.Inject;
 import java.util.Set;
 
-@Log4j2
-
+@AllArgsConstructor(onConstructor_ = {@Inject})
 public class RecipeManagerFacade {
-    private final RecipeLogic recipeLogic;
 
-    @Inject
-    public RecipeManagerFacade(RecipeLogic recipeLogic) {
-        this.recipeLogic = recipeLogic;
-    }
+    private final RecipeLogic recipeLogic;
 
     public void saveRecipe(Recipe recipe) {
         recipeLogic.saveRecipe(recipe);
     }
     public Set<Recipe> searchRecipeWithPartOfName(String textToLookFor) {
+        //ToDo: SearchLogic implementation
         recipeLogic.searchRecipeWithPartOfName(textToLookFor);
+        return null;
     }
 }
