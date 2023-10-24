408d366a703c9f92fd0a8effc8ab20939c83568a
DominicBürgin <dominic_bue@gmx.ch>
2023-10-24 21:17:28 +0200

    Cookboook: Umstellung auf JEE mit ear-Modul, 1. mal auf Tomee Deploybar

---
+++ homesite-ui/src/Test/java/domin/homesite/ui/DummyTest.java
@@ -0,0 +1,13 @@
+package domin.homesite.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
