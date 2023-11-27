53e788d5662a61eb283dad2c141e11b8ad0c6635
DominicBürgin <dominic_bue@gmx.ch>
2023-11-27 11:00:23 +0100

    homesite-gi: Changed type of attributes from Set to List

--- homesite-gil/src/main/java/domin/homesite/gil/domain/RecipeData.java
+++ homesite-gil/src/main/java/domin/homesite/gil/domain/RecipeData.java
@@ -3,13 +3,13 @@ package domin.homesite.gil.domain;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Builder(toBuilder = true)
 public class RecipeData {
 
-    private Set<Ingredient> ingredients;
-    private Set<Instruction> instructions;
+    private List<Ingredient> ingredients;
+    private List<Instruction> instructions;
 
 }
